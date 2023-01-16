e4c78eec87f739f86fdaf7758ee9172ea6787a23
YHara_Mac <Unity.dev.Agosan@gmail.com>
2023-01-16 16:10:44 +0900

    sagyou tyuu

--- Assets/Scripts/System/SystemManager.cs
+++ Assets/Scripts/System/SystemManager.cs
@@ -7,7 +7,17 @@ public class SystemManager : MonoBehaviour
 {
     public void SignChoosed(handsign choosedSign)
     {
+        int choosedSignID = (int)choosedSign;
         // ゆくゆくは相手の手は、他のプレイヤーの手によって決めたい
-        int opponentSign = Random.Range(0, 3);
+        int opponentSignID = Random.Range(0, 3);
+
+        int resultID = JankenJadge(choosedSignID, opponentSignID);
+
+        Debug.Log(resultID);
+    }
+    int JankenJadge(int own, int opponent)
+    {
+        // ジャンケンのアルゴリズム
+        return (own - opponent + 3) % 3;
     }
 }
