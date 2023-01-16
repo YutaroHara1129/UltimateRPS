1bec2c3e205848dc2361d7d89c8bca77d0689dbe
YHara_Win <Unity.dev.Agosan@gmail.com>
2023-01-16 22:41:06 +0900

    Add Feature / referee system

--- Assets/Scripts/System/SystemManager.cs
+++ Assets/Scripts/System/SystemManager.cs
@@ -5,15 +5,37 @@ using RPS;
 
 public class SystemManager : MonoBehaviour
 {
-    public void SignChoosed(handsign choosedSign)
+    private handsign ownHandSign;
+    private handsign opponentHandSign;
+
+    public void SignChoosed(int choosedSignID)
     {
-        int choosedSignID = (int)choosedSign;
         // ゆくゆくは相手の手は、他のプレイヤーの手によって決めたい
         int opponentSignID = Random.Range(0, 3);
 
+        ownHandSign = (handsign)System.Enum.ToObject(typeof(handsign), choosedSignID);
+        opponentHandSign = (handsign)System.Enum.ToObject(typeof(handsign), opponentSignID);
+
         int resultID = JankenJadge(choosedSignID, opponentSignID);
 
-        Debug.Log(resultID);
+        switch (resultID)
+        {
+            case (0):
+                Debug.Log("result : draw | ChoosedSign : " + ownHandSign
+                    + " | OpponentSign : " + opponentHandSign);
+                break;
+            case (1):
+                Debug.Log("result : win | ChoosedSign : " + ownHandSign
+                    + " | OpponentSign : " + opponentHandSign);
+                break;
+            case (2):
+                Debug.Log("result : defeat | ChoosedSign : " + ownHandSign
+                    + " | OpponentSign : " + opponentHandSign);
+                break;
+            default:
+                Debug.LogWarning("resultID is an invalid value. | value : " + resultID);
+                break;
+        }
     }
     int JankenJadge(int own, int opponent)
     {
